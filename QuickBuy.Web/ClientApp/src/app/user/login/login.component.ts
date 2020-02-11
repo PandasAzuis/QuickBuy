398d706bba0444211ee79fe2086be181dc1a39f1
Nicoly Melo <nicoly.melo18@outlook.com>
2020-02-11 09:19:06 -0300

    Logging on continues where it left off

--- QuickBuy.Web/ClientApp/src/app/user/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/user/login/login.component.ts
@@ -1,24 +1,29 @@
-import { Component } from "@angular/core"
+import { Component, OnInit } from "@angular/core"
 import { User } from "../../model/user";
-import { Router } from "@angular/router";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
   public user;
+  public returnUrl: string;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
     this.user = new User();
+    this.returnUrl = this.activatedRoute.snapshot.queryParams['returnUrl'];
   }
 
   login() {
     if (this.user.email == "nicoly.melo18@outlook.com" && this.user.password == "0") {
       sessionStorage.setItem("authenticated-user", "1");
-      this.router.navigate(['/']);
+      this.router.navigate([this.returnUrl]);
     }
   }
 
